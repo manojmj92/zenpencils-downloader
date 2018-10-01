54aeb52b78142840fbdb731981e1df1a1518ee90
garybsantos@gmail.com <garybsantos@gmail.com>
2018-10-01 17:08:48 -0400

    Comics download correctly, stops at latest comic

--- zenpencils.py
+++ zenpencils.py
@@ -13,33 +13,30 @@ from bs4 import BeautifulSoup
 import urllib2
 import os
 import sys
+import time
 
 dir = os.path.dirname(os.path.abspath(__file__))
-zendir = dir +"\\ZenComics"
+zendir = os.path.join(dir, "ZenComics")
 
 if not os.path.exists(zendir):
         os.makedirs(zendir)
 
-for url_range in range(1,145):
-
-    main_url = "http://zenpencils.com/comic/" + str(url_range)
+count = 0;
+while(1):
+    main_url = "http://zenpencils.com/comic/" + str(count)
     headers = {'User-agent': 'Mozilla/5.0'}
 
     main_url_opener = urllib2.Request(main_url, None, headers)
-    main_url_response = urllib2.urlopen(main_url_opener).read()
+    try:
+        main_url_response = urllib2.urlopen(main_url_opener).read()
 
-    main_url_soup = BeautifulSoup(main_url_response,'html.parser')
-
-
-    for comiclink in main_url_soup.find_all('img'):
-        all_links = comiclink.get('src')
-
-        if all_links.split('/')[3] == 'comics':
-            filename = all_links.split('/')[4]
-            res = urllib2.Request(all_links,None,headers)
-            img= urllib2.urlopen(res).read()
-            with open(zendir+"\\"+filename,"wb")as image:
-                    image.write(img)
-            print "Completed Download of  :"+filename
-            break
-
+        main_url_soup = BeautifulSoup(main_url_response,'html.parser')
+        images = main_url_soup.findAll("div", {"id": "comic"})
+        image = images[0].img['src']
+        name = image.split('/')[-1]
+        with open(os.path.join(zendir, name), 'wb') as f:
+            f.write(urllib2.urlopen(image.encode('utf-8')).read())
+    except urllib2.HTTPError:
+        print "Complete: " + str(count) + " images downloaded."
+        break
+    count += 1
